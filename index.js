5327d4619118a113b1ccb99638472156e1608647
saad <redpood92@gmail.com>
2024-02-12 16:42:49 +0500

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const http= require('http');
-const hostname = "localhost";
-const port = 3000;
-const server=http.createServer((req,res) =>{
+// const http= require('http');
+// const hostname = "localhost";
+// const port = 3000;
+// const server=http.createServer((req,res) =>{
 
-    console.log(req.headers);
-});
-server.listen(port,hostname, ()=>{
+//     console.log(req.headers);
+// });
+// server.listen(port,hostname, ()=>{
 
-});sessionStoragesdsd
-sdsd
+// });sessionStoragesdsd
+// sdsd
+var b = 2;
+const a = require("fs");
+
+//a.writeFileSync("./new1.txt",b.toLocaleString()+"/n");
+a.appendFileSync("./new1.txt",Date.now().toLocaleString()+"\n");
---
+++ practice.js
@@ -0,0 +1,10 @@
+var b; // Declare b as a global variable
+
+function a() {
+    b = 2 + 2;
+    return b;
+}
+
+module.exports=a
+
+
